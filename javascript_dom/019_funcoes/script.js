ff0b0a93b3adb6ab081e7e5f330ac404f8f78ef4
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-04-17 17:42:12 -0300

    Inicio funcoes

---
+++ javascript_dom/019_funcoes/script.js
@@ -0,0 +1,22 @@
+// FUNCTIONS
+
+// grupo de declarações que desempenham tarefas específicas, podem ser separads do fluxo principal do programa;
+
+/*
+function nome_da_funcao(){
+    código executado dentro da função
+}
+*/
+
+function escrever() {
+    console.log('Aqui está a frase da função.')
+}
+
+escrever();
+
+function escrever2(mensagem) {
+    console.log(mensagem);
+}
+
+escrever2("Olá Mundo!");
+escrever2("As funções são fáceis de criar");
