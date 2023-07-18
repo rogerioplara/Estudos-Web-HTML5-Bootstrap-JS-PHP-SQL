6c5c9526658b580a5a84f145c00286b3f12995c5
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-07-18 16:24:26 -0300

    Exercícios js

---
+++ javascript_exercicios/exercicio_003/app.js
@@ -0,0 +1,12 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 003
+Enunciado: CONTADOR COM CORES PARA NEGATIVOS, ZERO E POSITIVOS
+    
+    A partir do HTML existente, apresentar um valor inicial igual a 0 e definir
+    funcionalidades nos botões de decremento e incremento.
+    No caso do valor ser zero o texto deve ser branco.
+    No caso do valor ser negativo o texto deve ser vermelho puro.
+    No caso do valor ser positivo o texto deve ser verde puro.
+
+---------------------------------------------------------------------------- */
